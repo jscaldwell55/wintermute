2883a9bffc843a5918e956c20e49e03238d48d42
Jscaldwell55 <jay.s.caldwell@gmail.com>
2025-01-25 01:43:01 -0600

    upddd

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 
 # Core components - Imports assuming the structure in the image
 from api.core.memory.memory import MemorySystem, MemoryType
-from api.core.vector_operations import MockVectorOperations, VectorOperations
+from api.core.vector_operations import VectorOperations
 from api.utils.context_window import ContextWindow
 from api.utils.dream_sequence import DreamSequence
 from api.utils.pinecone_service import PineconeService
